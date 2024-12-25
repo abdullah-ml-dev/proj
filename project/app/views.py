b296a2c0c2c5dce16aca7bb618c490ee8b388a41
Abdullah <abdullah8j03@gmail.com>
2024-12-25 13:17:13 +0500

    dic key rendering to var in html

--- project/app/views.py
+++ project/app/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    print("Hello to the index page")
+    welcome = "Welcome to the index page"
+    return render(request, "index.html",{ "greetings": welcome})
