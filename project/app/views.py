1521d4153e887cb00ac9b5cd789bebf179f44c41
Abdullah <abdullah8j03@gmail.com>
2024-12-25 15:11:14 +0500

    index & contact page planed structure with out database

--- project/app/views.py
+++ project/app/views.py
@@ -5,3 +5,6 @@ def index(request):
     print("Hello to the index page")
     welcome = "Welcome to the index page"
     return render(request, "index.html",{ "greetings": welcome})
+def contact(request):
+    print("Hello to the contact page")
+    return render(request, "contact.html")
